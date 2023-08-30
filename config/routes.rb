c9b6abce3f4a4208ef3192b338aadaa6eb05af42
Christopher A Paredes López <topher98.dev@gmail.com>
2023-08-30 00:25:45 -0400

    API

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants
+      resources :devices
+    end
+  end
 
 end
