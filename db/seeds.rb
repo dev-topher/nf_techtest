1cd8327f986c427b929a68fc8bbe6e8622d77c35
Christopher A Paredes López <topher98.dev@gmail.com>
2023-08-30 21:28:37 -0400

    Seed de base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+restaurants = [
+  { id: 1, name: 'Restaurante Alpha'},
+  { id: 2, name: 'Restaurante Beta'}
+]
+
+Restaurant.create(restaurants)
+
+devices = [
+  { id: 1, name: 'Impresora', status: true, restaurant_id: 1},
+  { id: 2, name: 'Servidor web', status: true, restaurant_id: 1},
+  { id: 3, name: 'Servidor de base de datos', status: true, restaurant_id: 1},
+  { id: 4, name: 'Impresora', status: true, restaurant_id: 2},
+  { id: 5, name: 'Servidor web', status: true, restaurant_id: 2},
+  { id: 6, name: 'Servidor de base de datos', status: true, restaurant_id: 2}
+]
+
+Device.create(devices)
